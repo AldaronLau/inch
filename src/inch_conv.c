6dfb03c9b91ac8b6febed581f479c29d39f2f8c5
jeron <deadbeef.jal@gmail.com>
2016-07-04 14:14:15 -0500

    Updated so header folder isn't used anymore - now include folder is used.

--- src/inch_conv.c
+++ src/inch_conv.c
@@ -4,7 +4,7 @@
  * INCH: INclude C Header
 */
 
-#include "header/inch.h"
+#include "include/inch.h"
 
 static inline void inch_conv_add(jl_t* jl, const char* printed) {
 //	jl_print(jl, "CONV ADD...");
@@ -73,7 +73,7 @@ void inch_conv_conv(jl_t* jl, const char *PackageName) {
 
 void inch_conv_save(jl_t* jl) {
 	inch_t* ctx = jl_get_context(jl);
-	char save[100]; jl_mem_format(save, "!src/header/%s.h", ctx->fp_filename);
+	char save[100]; jl_mem_format(save, "!src/include/%s.h", ctx->fp_filename);
 	char* output = jl_data_tostring(jl, &ctx->output);
 
 	jl_print_rewrite(jl, "Saving output to \"%s\" [....]", save);
